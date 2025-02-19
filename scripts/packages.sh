d5c917791d2237d9eecd76fa9a582bebf45ec6a9
liuhrme <102905159+liuhrme@users.noreply.github.com>
2025-02-19 09:58:46 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -35,9 +35,9 @@ pushd package/small
 #ssrp
 # git clone -b master --depth 1 https://github.com/fw876/helloworld.git
 #passwall
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 #mosdns
 # git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 #openclash
